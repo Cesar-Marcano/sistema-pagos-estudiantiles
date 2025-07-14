4269db61f2a1692b1a04322b1336dab0194b6b02
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-14 18:23:46 -0400

    feat: implement router

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import "./config/loadEnv";
 
 import express from "express";
 
+import { router } from "./router";
+
 const app = express();
+
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Server on port 3000");
