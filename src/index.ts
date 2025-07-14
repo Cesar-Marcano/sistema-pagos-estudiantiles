02bf83d62c2a38c09ee872689d17f98b4b1ffeb7
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-14 18:41:17 -0400

    refactor: reorganize middleware imports and usage in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,26 @@
 import "./config/loadEnv";
 
 import express from "express";
+import morgan from "morgan";
 
 // local imports
 import { router } from "./router";
+import { executeInDev } from "./config/envVariables";
+import helmet from "helmet";
 
 // express instance
 const app = express();
 
+// middlewares
+app.use(helmet());
+
+// dev middlewares
+executeInDev(() => {
+  app.use(morgan("dev"));
+});
+
 // router
-app.use('/api', router);
+app.use("/api", router);
 
 // express start
 app.listen(3000, () => {
