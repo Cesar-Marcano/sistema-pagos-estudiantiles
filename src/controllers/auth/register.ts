268e07065dc4fda4edc8057c6e61d84224d6f4b9
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-15 17:27:11 -0400

    feat: update register controller to include AuthService for refresh token generation

--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -2,18 +2,29 @@ import { validateBodyWrapper } from "../../utils/validateBodyWrapper";
 import { CreateUserSchema } from "../../dtos/createUser.dto";
 import { UserService } from "../../services/user.service";
 import { executeInDev } from "../../config/envVariables";
+import { AuthService } from "../../services/auth.service";
 
-export const registerContrller = (userService: UserService) =>
+export const registerContrller = (
+  userService: UserService,
+  authService: AuthService
+) =>
   validateBodyWrapper(CreateUserSchema, async (req, res) => {
     try {
       const user = await userService.createUser(req.body);
 
-      res.status(201).json(user);
+      const refreshToken = authService.retrieveRefreshToken({
+        id: user.id,
+        name: user.name,
+        role: user.role,
+        username: user.name,
+      });
+
+      res.status(201).json({ refreshToken });
     } catch (error) {
       executeInDev(() => {
         res.status(500).json(error);
       });
-      
+
       res.status(500).json({ error: "Internal server error" });
       return;
     }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,9 +3,13 @@ import { Router } from "express";
 import { registerContrller } from "../controllers/auth/register";
 import { prisma } from "../prisma";
 import { UserService } from "../services/user.service";
+import { AuthService } from "../services/auth.service";
 
 export const authRouter = Router();
 
+// services
 const userService = new UserService(prisma);
+const authService = new AuthService();
 
-authRouter.post("/register", registerContrller(userService));
+// routes initialization
+authRouter.post("/register", registerContrller(userService, authService));
