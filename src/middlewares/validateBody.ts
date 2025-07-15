9249977b9c5408db85510f59ff719bd9da105f8d
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-15 12:40:59 -0400

    feat: add validateBodyMiddleware for request body validation

---
+++ src/middlewares/validateBody.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodAny, ZodError } from "zod";
+import { executeInDev } from "../config/envVariables";
+
+export const validateBodyMiddlware = (schema: ZodAny) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const parsedBody = await schema.parseAsync(req.body);
+
+      req.body = parsedBody;
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res.status(400).json({
+          message: error.message,
+        });
+        return;
+      }
+
+      res.status(500);
+
+      executeInDev(() => {
+        res.json({ error });
+      });
+    }
+  };
+};
