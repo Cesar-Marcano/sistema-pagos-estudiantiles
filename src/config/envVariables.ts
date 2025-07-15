4aa183d98f2bc141d866e70baf9f10e3de6c89fc
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-15 17:16:38 -0400

    feat: add token expiration constants for refresh and access tokens

--- src/config/envVariables.ts
+++ src/config/envVariables.ts
@@ -1,9 +1,19 @@
 export const { NODE_ENV } = process.env;
 
+export const REFRESH_TOKEN_EXP = "2d";
+
+export const ACCESS_TOKEN_EXP = "15m";
+
 export const isDevelopment = () => NODE_ENV === "development";
 
 export function executeInDev(callback: () => void) {
   if (isDevelopment()) {
     callback();
   }
+}
+
+export function getEnv<T>(key: string, defaultVal?: T) {
+  const val = process.env[key];
+
+  return (val ? val : defaultVal) as T;
 }
