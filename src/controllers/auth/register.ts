1373705ffee5dd0ddad7dbfcf1c256755f0a5bb5
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-15 14:03:00 -0400

    feat: implement register controller with user creation logic

--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -1,9 +1,20 @@
 import { validateBodyWrapper } from "../../utils/validateBodyWrapper";
 import { CreateUserSchema } from "../../dtos/createUser.dto";
+import { UserService } from "../../services/user.service";
+import { executeInDev } from "../../config/envVariables";
 
-export const registerContrller = validateBodyWrapper(
-  CreateUserSchema,
-  (req, res) => {
-    res.send("hello");
-  }
-);
+export const registerContrller = (userService: UserService) =>
+  validateBodyWrapper(CreateUserSchema, async (req, res) => {
+    try {
+      const user = await userService.createUser(req.body);
+
+      res.status(201).json(user);
+    } catch (error) {
+      executeInDev(() => {
+        res.status(500).json(error);
+      });
+      
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+  });
