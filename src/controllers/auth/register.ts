34b9f74c5c0e88e68ff9fd5e3b2853d10dce0b94
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-15 13:40:51 -0400

    fix: remove unnecessary type annotations in register controller

--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -1,10 +1,9 @@
-import { Request, Response } from "express";
 import { validateBodyWrapper } from "../../utils/validateBodyWrapper";
 import { CreateUserSchema } from "../../dtos/createUser.dto";
 
 export const registerContrller = validateBodyWrapper(
   CreateUserSchema,
-  (req: Request, res: Response) => {
+  (req, res) => {
     res.send("hello");
   }
 );
