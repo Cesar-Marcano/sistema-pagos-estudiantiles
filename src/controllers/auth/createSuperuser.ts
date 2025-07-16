d8b7d54f099895b4919e4a3ebbcc3ff5f1b3255b
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-16 15:31:21 -0400

    feat: created create super user controller

---
+++ src/controllers/auth/createSuperuser.ts
@@ -0,0 +1,38 @@
+import { UserService } from "../../services/user.service";
+import { AuthService } from "../../services/auth.service";
+import { validateBodyMiddleware } from "../../middlewares/validateBody";
+import { Controller, Handler, Middleware } from "../../utils/controller";
+import {
+  CreateSuperUserDto,
+  CreateSuperUserSchema,
+} from "../../dtos/createSuperUser.dto";
+
+export class CreateSuperUserController extends Controller<CreateSuperUserDto> {
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService
+  ) {
+    super();
+  }
+
+  public middlewares: Middleware[] = [
+    validateBodyMiddleware(CreateSuperUserSchema),
+  ];
+
+  public handler: Handler<CreateSuperUserDto> = async (req, res) => {
+    const user = await this.userService.createSuperuser(
+      req.body.username,
+      req.body.password,
+      req.body.name
+    );
+
+    const refreshToken = this.authService.retrieveRefreshToken({
+      id: user.id,
+      name: user.name,
+      role: user.role,
+      username: user.name,
+    });
+
+    res.status(201).json({ refreshToken });
+  };
+}
