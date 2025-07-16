9deca2ca937bdc06d40a162aa94705ae0f9f7d5d
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-16 15:33:38 -0400

    feat: added new controllers to auth router

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,6 +6,8 @@ import { UserService } from "../services/user.service";
 import { AuthService } from "../services/auth.service";
 import { LoginController } from "../controllers/auth/login";
 import { AccessTokenController } from "../controllers/auth/accessToken";
+import { CreateSuperUserController } from "../controllers/auth/createSuperuser";
+import { NeedsSuperUserController } from "../controllers/auth/needsSuperUser";
 
 export const authRouter = Router();
 
@@ -17,8 +19,16 @@ const authService = new AuthService();
 const registerController = new RegisterController(userService, authService);
 const loginController = new LoginController(userService, authService);
 const accessTokenController = new AccessTokenController(authService);
+const createSuperUserController = new CreateSuperUserController(
+  userService,
+  authService
+);
+const needsSuperUser = new NeedsSuperUserController(userService);
 
 // routes initialization
 authRouter.post("/register", registerController.build());
 authRouter.post("/login", loginController.build());
 authRouter.post("/access", accessTokenController.build());
+authRouter.post("/create-super-user", createSuperUserController.build());
+
+authRouter.get("/needs-super-user", needsSuperUser.build());
