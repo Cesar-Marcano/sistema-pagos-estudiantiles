e6aad9f23a1469ef771c1ae1381ef279704faa2f
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-24 15:45:20 -0400

    feat: created internal server error custom error class

---
+++ src/errors/internalServer.error.ts
@@ -0,0 +1,8 @@
+import { i18n } from "../lang/i18n";
+import { BaseError } from "../utils/baseError";
+
+export class InternalServerError extends BaseError {
+  constructor() {
+    super(i18n`errors.internal_server_error`, 500);
+  }
+}
