d8e31afc94e433768b3d4fcd9c7c081bb4365e8c
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-24 12:14:46 -0400

    feat(i18n): add new error message keys for authentication module

--- src/lang/languages/en.ts
+++ src/lang/languages/en.ts
@@ -1,4 +1,5 @@
 export const englishTranslations: { [key: string]: string } = {
   "errors.unauthorized": "Unauthorized",
   "errors.unauthorized.details": "Unauthorized: {0}",
+  "errors.unauthorized.rolemismatch": "Role mismatch",
 };
--- src/lang/languages/sp.ts
+++ src/lang/languages/sp.ts
@@ -1,4 +1,5 @@
 export const spanishTranslations: { [key: string]: string } = {
   "errors.unauthorized": "No autorizado",
   "errors.unauthorized.details": "No autorizado: {0}",
+  "errors.unauthorized.rolemismatch": "Rol incorrecto",
 };
