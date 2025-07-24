c277cdb4e31a988f3b9323fdaa8fc4b2e550d927
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-24 12:15:43 -0400

    feat: updated auth guard to use i18n

--- src/middlewares/authGuard.ts
+++ src/middlewares/authGuard.ts
@@ -2,13 +2,14 @@ import { Role } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { AuthService, UserPayload } from "../services/auth.service";
 import { UnauthorizedError } from "../errors/unauthorized.error";
+import { i18n } from "../lang/i18n";
 
 interface AuthenticatedRequest extends Request {
   user?: UserPayload;
 }
 
 export const authGuard = (authService: AuthService, role?: Role) => {
-  return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  return (req: AuthenticatedRequest, _res: Response, next: NextFunction) => {
     const authHeader = req.header("Authorization");
     const token = authHeader?.split(" ")[1];
 
@@ -19,7 +20,7 @@ export const authGuard = (authService: AuthService, role?: Role) => {
     const payload = authService.decodeAccessToken(token);
 
     if (role !== payload.role) {
-      throw new UnauthorizedError("Role mismatch");
+      throw new UnauthorizedError(i18n`errors.unauthorized.rolemismatch`);
     }
 
     if (!payload) {
