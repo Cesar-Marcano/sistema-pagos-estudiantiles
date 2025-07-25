f85bb82250a53a58eed7d3dfddaf179978c5736a
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-25 18:41:29 -0400

    feat(parent-service): created controller to update parent data

---
+++ src/controllers/parent/updateParent.ts
@@ -0,0 +1,42 @@
+import { AuthService } from "../../services/auth.service";
+import { validateBodyMiddleware } from "../../middlewares/validateBody";
+import { Controller, Handler, Middleware } from "../../utils/controller";
+import { authGuard } from "../../middlewares/authGuard";
+import { Role } from "@prisma/client";
+import {
+  UpdateParentDto,
+  UpdateParentSchema,
+} from "../../dtos/updateParent.dto";
+import { ParentService } from "../../services/parent.service";
+import { UserPayload } from "../../interfaces/tokenPayload";
+import z from "zod";
+
+export class UpdateParentController extends Controller<
+  UpdateParentDto,
+  UserPayload
+> {
+  constructor(
+    private readonly parentService: ParentService,
+    private readonly authService: AuthService
+  ) {
+    super();
+  }
+
+  public middlewares: Middleware[] = [
+    authGuard(this.authService, Role.ADMIN),
+    validateBodyMiddleware(UpdateParentSchema),
+  ];
+
+  public handler: Handler<UpdateParentDto, UserPayload> = async (req, res) => {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().min(1),
+    });
+
+    const { id: parentId } = paramsSchema.parse(req.params);
+    const updateData = req.body;
+
+    const parent = await this.parentService.updateParent(parentId, updateData);
+
+    res.status(200).json({ parent });
+  };
+}
--- src/routes/parent.ts
+++ src/routes/parent.ts
@@ -4,6 +4,7 @@ import { prisma } from "../prisma";
 import { AuthService } from "../services/auth.service";
 import { ParentService } from "../services/parent.service";
 import { CreateParentController } from "../controllers/parent/createParent";
+import { UpdateParentController } from "../controllers/parent/updateParent";
 
 export const parentRouter = Router();
 
@@ -16,6 +17,11 @@ const createParentController = new CreateParentController(
   parentService,
   authService
 );
+const updateParentController = new UpdateParentController(
+  parentService,
+  authService
+);
 
 // routes initialization
 parentRouter.post("/", createParentController.build());
+parentRouter.patch("/:id", updateParentController.build());
