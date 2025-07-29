6324d215b31f18412791c8a64c080e83eb933c12
Cesar-Marcano <marcanocesar86@gmail.com>
2025-07-29 18:33:27 -0400

    feat: created audit logs service

---
+++ src/services/auditLogs.service.ts
@@ -0,0 +1,32 @@
+import { AuditLog, PrismaClient } from "@prisma/client";
+
+export class AuditLogsService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  public async registerLog(data: AuditLog) {
+    return await this.prisma.auditLog.create({
+      data,
+    });
+  }
+
+  public async getLogs(page: number = 1, pageSize: number = 10) {
+    const skip = (page - 1) * pageSize;
+    const take = pageSize;
+
+    return await this.prisma.auditLog.findMany({
+      skip,
+      take,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
+
+  public async getLog(logId: number) {
+    return await this.prisma.auditLog.findUnique({
+      where: {
+        id: logId,
+      },
+    });
+  }
+}
