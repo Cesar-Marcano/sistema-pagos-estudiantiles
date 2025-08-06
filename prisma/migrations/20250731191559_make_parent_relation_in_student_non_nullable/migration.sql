a452e4e6ab92764a207db7a5a1868d3da2747b4c
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-06 18:37:05 -0400

    fix: adjust Prisma auto migration to preserve indexes

--- prisma/migrations/20250731191559_make_parent_relation_in_student_non_nullable/migration.sql
+++ prisma/migrations/20250731191559_make_parent_relation_in_student_non_nullable/migration.sql
@@ -8,31 +8,31 @@
 ALTER TABLE "Student" DROP CONSTRAINT "Student_parentId_fkey";
 
 -- DropIndex
-DROP INDEX "Discount_name_deletedAt_key";
+-- DROP INDEX "Discount_name_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "Grade_name_deletedAt_key";
+-- DROP INDEX "Grade_name_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "Invoice_invoiceNumber_deletedAt_key";
+-- DROP INDEX "Invoice_invoiceNumber_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "Parent_document_deletedAt_key";
+-- DROP INDEX "Parent_document_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "Parent_email_deletedAt_key";
+-- DROP INDEX "Parent_email_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "Parent_phoneNumber_deletedAt_key";
+-- DROP INDEX "Parent_phoneNumber_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "Payment_referenceNumber_deletedAt_key";
+-- DROP INDEX "Payment_referenceNumber_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "PaymentMethod_name_deletedAt_key";
+-- DROP INDEX "PaymentMethod_name_deletedAt_key";
 
 -- DropIndex
-DROP INDEX "Student_document_deletedAt_key";
+-- DROP INDEX "Student_document_deletedAt_key";
 
 -- AlterTable
 ALTER TABLE "Student" ALTER COLUMN "parentId" SET NOT NULL;
