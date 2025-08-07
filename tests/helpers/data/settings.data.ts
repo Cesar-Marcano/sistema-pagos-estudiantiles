217b329d643073af3cdef82d03669fb50ba11ff4
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-07 18:09:03 -0400

    tests: created test for getting settings

---
+++ tests/helpers/data/settings.data.ts
@@ -0,0 +1,7 @@
+import { Setting } from "@prisma/client";
+
+export const sampleSetting: Setting = {
+  id: 1,
+  name: "lang",
+  value: "en",
+};
---
+++ tests/unit/services/settings.service.spec.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { SettingsService } from "../../../src/services/settings.service";
+import { createMockPrisma } from "../../helpers/factories/prisma.factory";
+import { sampleSetting } from "../../helpers/data/settings.data";
+
+describe("SettingsService", () => {
+  let prisma: PrismaClient;
+  let settingsService: SettingsService;
+
+  beforeEach(() => {
+    prisma = createMockPrisma(["setting"], {
+      setting: {
+        upsert: jest.fn().mockResolvedValue(sampleSetting),
+        findUnique: jest.fn().mockResolvedValue(sampleSetting),
+      },
+    });
+    settingsService = new SettingsService(prisma);
+  });
+
+  it("should get a setting", async () => {
+    const setting = await settingsService.getConfig("lang", "something");
+
+    expect(setting).toEqual(sampleSetting.value);
+  });
+
+  it("should get a default setting", async () => {
+    (prisma.setting.findUnique as jest.Mock) = jest
+      .fn()
+      .mockResolvedValue(null);
+
+    const setting = await settingsService.getConfig("lang", "something");
+
+    expect(setting).toEqual("something");
+  });
+});
