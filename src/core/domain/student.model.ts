34aad12014f4948d13a6856fbfbef6ce2c57c13a
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-10 17:55:22 -0400

    feat(domain): add Student model with status and grade interfaces

---
+++ src/core/domain/student.model.ts
@@ -0,0 +1,42 @@
+import { Grade, GradeWithLevels, GradeWithoutLevels } from "./grade.model";
+import { Parent } from "./parent.model";
+import { User } from "./user.model";
+
+export enum StudentStatus {
+  Active,
+  Inactive,
+}
+
+interface RawStudent {
+  id: number;
+  fullname: string;
+  birthday: Date;
+  section?: string;
+  document?: string;
+  status: StudentStatus;
+  
+  grade: number | Grade;
+  gradeLevel?: number | never;
+
+  parent: number | Parent;
+
+  createdBy: number | User;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
+
+export interface StudentWithGradeWithoutLevels extends RawStudent {
+  grade: GradeWithoutLevels;
+  gradeLevel?: never;
+}
+
+export interface StudentWithGradeWithLevels extends RawStudent {
+  grade: GradeWithLevels;
+  gradeLevel: number;
+}
+
+export type Student =
+  | StudentWithGradeWithLevels
+  | StudentWithGradeWithoutLevels;
