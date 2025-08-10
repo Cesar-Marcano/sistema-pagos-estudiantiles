f4b37b1610ea9dea23340a93f2608e8824aa4b04
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-10 17:28:11 -0400

    feat(domain): created user model

---
+++ src/core/domain/user.model.ts
@@ -0,0 +1,16 @@
+import { Role } from "@prisma/client";
+
+export interface User {
+  id: number;
+  role: number | Role;
+  username: string;
+  name: string;
+  password: string;
+  email: string;
+
+  createdBy: number | User;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+}
