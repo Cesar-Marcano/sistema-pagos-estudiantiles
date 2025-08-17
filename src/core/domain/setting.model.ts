e745a6f16cadaab3b48caef846810a1642b86ede
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-17 17:56:26 -0400

    feat(core): migrated setting to class

--- src/core/domain/setting.model.ts
+++ src/core/domain/setting.model.ts
@@ -1,5 +1,38 @@
-export interface Setting {
-  id: number;
-  name: string;
-  value: string;
+export class Setting {
+  constructor(
+    private _name: string,
+    private _value: string,
+    private readonly _id?: number
+  ) {}
+
+  public static create(_name: string, _value: string): Setting {
+    if (_value.trim().length < 1) {
+      throw new Error("Empty setting value");
+    }
+
+    return new Setting(_name, _value);
+  }
+
+  public get id(): number | undefined {
+    return this._id;
+  }
+
+  public get value(): string {
+    return this._value;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public updateValue(val: string): this {
+    const settingVal = val.trim();
+    if (settingVal.length < 1) {
+      throw new Error("Empty setting value");
+    }
+
+    this._value = settingVal;
+
+    return this;
+  }
 }
