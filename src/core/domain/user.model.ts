966f34e42da10b761fdd9c79389a4e98c31f22b4
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 16:19:08 -0400

    refactor(core): moved hasher service

--- src/core/domain/user.model.ts
+++ src/core/domain/user.model.ts
@@ -1,7 +1,7 @@
 import { Email } from "../datavalues/email.datavalue";
 import { Password } from "../datavalues/password.datavalue";
 import { Username } from "../datavalues/username.datavalue";
-import { IHasherService } from "../ports/out/hasher.port";
+import { IHasherService } from "../ports/out/services/hasher.port";
 import { Role } from "./role.model";
 
 export class User {
