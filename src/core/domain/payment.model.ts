21a479d5a2b24c75bac7076f2621269932f2eb7d
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 14:13:45 -0400

    feat(payment): migrate payment model to class

--- src/core/domain/payment.model.ts
+++ src/core/domain/payment.model.ts
@@ -2,83 +2,259 @@ import { Invoice } from "./invoice.model";
 import { PaymentMethod } from "./paymentMethod.model";
 import { User } from "./user.model";
 
-interface BasePayment {
-  id: number;
-  amount: number;
-  denied: boolean;
-  notes?: string;
+export class Payment {
+  constructor(
+    private _amount: number,
+    private _denied: boolean,
+    private _notes: string | null,
 
-  paymentMethod: number | PaymentMethod;
+    private _paymentMethod: number | PaymentMethod,
 
-  invoice: number | Invoice;
+    private _invoice: number | Invoice,
 
-  verified?: boolean;
-  verifiedAt?: Date;
-  verifiedBy?: number | User;
+    private _verified: boolean | null,
+    private _verifiedAt: Date | null,
+    private _verifiedBy: (number | User) | null,
 
-  referenceNumber?: string;
+    private _referenceNumber: string | null,
 
-  createdBy: number | User;
+    private _createdBy: number | User,
 
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt: Date | null;
+    private _createdAt: Date,
+    private _updatedAt: Date,
+    private _deletedAt: Date | null,
+    private readonly _id?: number
+  ) {}
+
+  public static create(
+    _amount: number,
+    _notes: string | null,
+
+    _paymentMethod: PaymentMethod,
+
+    _invoice: number | Invoice,
+
+    _verified: boolean | null,
+    _verifiedAt: Date | null,
+    _verifiedBy: (number | User) | null,
+
+    _referenceNumber: string | null,
+
+    _createdBy: number | User
+  ) {
+    if (_amount < 1) {
+      throw new Error("Invalid amount");
+    }
+
+    const notes = _notes?.trim() ?? null;
+    const referenceNumber = _referenceNumber?.trim() ?? null;
+
+    if (notes && notes.length < 2) {
+      throw new Error("Invalid notes");
+    }
+
+    if (referenceNumber && referenceNumber.length < 2) {
+      throw new Error("Invalid reference number");
+    }
+
+    if (_paymentMethod instanceof PaymentMethod) {
+      if (_paymentMethod.requiresManualVerification && _verified === null) {
+        throw new Error("Verified needs to be true or false but not null");
+      }
+
+      if (_paymentMethod.requiresReferenceNumber && _referenceNumber === null) {
+        throw new Error("Reference number needs to be filled");
+      }
+
+      if (
+        !_paymentMethod.requiresReferenceNumber &&
+        _referenceNumber !== null
+      ) {
+        throw new Error("Reference number needs to be null");
+      }
+
+      if (
+        !_paymentMethod.requiresManualVerification &&
+        (_verified !== null || _verifiedAt !== null || _verifiedBy !== null)
+      ) {
+        throw new Error(
+          "Verified, verified at and verified by needs to be null"
+        );
+      }
+    }
+
+    const now = new Date();
+
+    return new Payment(
+      _amount,
+      false,
+      notes,
+      _paymentMethod,
+      _invoice,
+      _verified,
+      _verifiedAt,
+      _verifiedBy,
+      referenceNumber,
+      _createdBy,
+      now,
+      now,
+      null
+    );
+  }
+
+  public get id(): number | undefined {
+    return this._id;
+  }
+
+  public get amount(): number {
+    return this._amount;
+  }
+
+  public get denied(): boolean {
+    return this._denied;
+  }
+
+  public get notes(): string | null {
+    return this._notes;
+  }
+
+  public get paymentMethod(): number | PaymentMethod {
+    return this._paymentMethod;
+  }
+
+  public get invoice(): number | Invoice {
+    return this.invoice;
+  }
+
+  public get verified(): boolean | null {
+    return this._verified;
+  }
+
+  public get verifiedAt(): Date | null {
+    return this._verifiedAt;
+  }
+
+  public get verifiedBy(): (number | User) | null {
+    return this._verifiedBy;
+  }
+
+  public get referenceNumber(): string | null {
+    return this._referenceNumber;
+  }
+
+  public get createdBy(): number | User {
+    return this._createdBy;
+  }
+
+  public createdAt(): Date {
+    return this._createdAt;
+  }
+
+  public updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  public deletedAt(): Date | null {
+    return this._deletedAt;
+  }
+
+  public updateAmount(val: number): this {
+    if (val < 1) {
+      throw new Error("Invalid amount");
+    }
+
+    if (this._amount === val) return this;
+
+    this._amount = val;
+    this._updatedAt = new Date();
+
+    return this;
+  }
+
+  public updateDenied(val: boolean): this {
+    if (this._denied === val) {
+      return this;
+    }
+
+    this._denied = val;
+    this._updatedAt = new Date();
+
+    return this;
+  }
+
+  public updateNotes(val: string): this {
+    const notes = val.trim();
+
+    if (notes.length < 2) {
+      throw new Error("Invalid notes");
+    }
+
+    this._notes = notes;
+    this._updatedAt = new Date();
+
+    return this;
+  }
+
+  public updateVerified(
+    verified: false,
+    verifiedAt?: null,
+    verifiedBy?: null
+  ): this;
+
+  public updateVerified(
+    verified: true,
+    verifiedAt: Date,
+    verifiedBy: number | User
+  ): this;
+
+  public updateVerified(
+    verified: boolean,
+    verifiedAt?: Date | null,
+    verifiedBy?: (number | User) | null
+  ): this {
+    if (verified === false) {
+      if (this._verified === verified) {
+        return this;
+      }
+
+      this._verified = verified;
+      this._updatedAt = new Date();
+
+      return this;
+    }
+
+    if (!verifiedAt || !verifiedBy) {
+      throw new Error("Verified at and verified by are required");
+    }
+
+    this._verified = verified;
+    this._verifiedAt = verifiedAt;
+    this._verifiedBy = verifiedBy;
+
+    this._updatedAt = new Date();
+
+    return this;
+  }
+
+  public updateReferenceNumber(val: string): this {
+    const referenceNumber = val.trim();
+
+    if (referenceNumber.length < 2) {
+      throw new Error("Invalid reference number");
+    }
+
+    this._referenceNumber = referenceNumber;
+    this._updatedAt = new Date();
+
+    return this;
+  }
+
+  public delete(): this {
+    const now = new Date();
+
+    this._deletedAt = now;
+    this._updatedAt = now;
+
+    return this;
+  }
 }
-
-export interface PaymentFullControl extends BasePayment {
-  paymentMethod:
-    | number
-    | (PaymentMethod & {
-        requiresManualVerification: true;
-        requiresReferenceNumber: true;
-      });
-  verified: boolean;
-  verifiedAt: Date;
-  verifiedBy: number | User;
-  referenceNumber: string;
-}
-
-export interface PaymentWithVerification extends BasePayment {
-  paymentMethod:
-    | number
-    | (PaymentMethod & {
-        requiresManualVerification: true;
-        requiresReferenceNumber: false;
-      });
-  verified: boolean;
-  verifiedAt: Date;
-  verifiedBy: number | User;
-  referenceNumber?: never;
-}
-
-export interface PaymentWithReference extends BasePayment {
-  paymentMethod:
-    | number
-    | (PaymentMethod & {
-        requiresManualVerification: false;
-        requiresReferenceNumber: true;
-      });
-  verified?: never;
-  verifiedAt?: never;
-  verifiedBy?: never;
-  referenceNumber: string;
-}
-
-export interface PaymentWithoutControls extends BasePayment {
-  paymentMethod:
-    | number
-    | (PaymentMethod & {
-        requiresManualVerification: false;
-        requiresReferenceNumber: false;
-      });
-  verified?: never;
-  verifiedAt?: never;
-  verifiedBy?: never;
-  referenceNumber?: never;
-}
-
-export type Payment =
-  | PaymentFullControl
-  | PaymentWithVerification
-  | PaymentWithReference
-  | PaymentWithoutControls;
