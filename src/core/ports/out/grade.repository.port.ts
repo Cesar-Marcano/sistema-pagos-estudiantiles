2183dbb1a568ddd36374d6a0242f55c1555ddf13
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 19:49:20 -0400

    feat(grade): added grade repository port

---
+++ src/core/ports/out/grade.repository.port.ts
@@ -0,0 +1,29 @@
+import { Paginated } from "../../../shared/interfaces/paginated";
+import { Grade } from "../../domain/grade.model";
+
+export interface IGradeSearchCriteria {
+  query?: {
+    name?: string;
+    hasLevels?: boolean;
+    maxLevel?: number;
+    fee?: number;
+    createdById?: number;
+    createdAt?: Date;
+    deletedAt?: Date;
+  };
+  page: number;
+  pageSize: number;
+  includeDeleted: boolean;
+}
+
+export interface IGradeRepository {
+  create(grade: Grade): Promise<Grade>;
+
+  search(criteria: IGradeSearchCriteria): Promise<Paginated<Grade>>;
+  findById(id: number): Promise<Grade>;
+
+  upsert(grade: Grade): Promise<Grade>;
+
+  softDelete(id: number): Promise<Grade>;
+  restore(id: number): Promise<Grade>;
+}
