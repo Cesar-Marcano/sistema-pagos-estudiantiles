eefe5f96cdc9c892d56239cccff3b83db26f205d
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 12:54:24 -0400

    refactor(core): changes session to a class

--- src/core/domain/session.model.ts
+++ src/core/domain/session.model.ts
@@ -1,9 +1,31 @@
 import { User } from "./user.model";
 
-export interface Session {
-  id: number;
-  jti: string;
-  user: number | User;
+export class Session {
+  constructor(
+    private _jti: string,
+    private _user: number | User,
 
-  createdAt: Date;
+    private _createdAt: Date,
+    private readonly _id?: number
+  ) {}
+
+  public static create(_jti: string, _user: number | User): Session {
+    return new Session(_jti, _user, new Date());
+  }
+
+  public get id(): number | undefined {
+    return this._id;
+  }
+
+  public get jti(): string {
+    return this._jti;
+  }
+
+  public get user(): number | User {
+    return this._user;
+  }
+
+  public get createdAt(): Date {
+    return this._createdAt;
+  }
 }
