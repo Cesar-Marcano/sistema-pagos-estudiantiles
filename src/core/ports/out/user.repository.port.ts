79c73d5bf49cfa1a8f4b04d331316e706a29e6a4
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 19:18:02 -0400

    feat(user): added user search functionality

--- src/core/ports/out/user.repository.port.ts
+++ src/core/ports/out/user.repository.port.ts
@@ -1,4 +1,19 @@
+import { Paginated } from "../../../shared/interfaces/paginated";
 import { User } from "../../domain/user.model";
+
+export interface IUserSearchCriteria {
+  query?: {
+    id?: number;
+    username?: string;
+    name?: string;
+    email?: string;
+    createdAt?: Date;
+    deletedAt?: Date;
+  };
+  page: number;
+  pageSize: number;
+  includeDeleted: boolean;
+}
 
 export interface IUserRepository {
   create(user: User): Promise<User>;
@@ -9,6 +24,7 @@ export interface IUserRepository {
 
   findById(id: number): Promise<User>;
   findByEmail(email: string): Promise<User>;
+  search(criteria: IUserSearchCriteria): Promise<Paginated<User>>;
 
   softDelete(id: number): Promise<User>;
   restore(id: number): Promise<User>;
