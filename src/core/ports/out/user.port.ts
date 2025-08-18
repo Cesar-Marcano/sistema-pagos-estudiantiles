1792f75d719235b47daf8434219bcac9439b3bb0
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 16:41:45 -0400

    feat(user): added user repository port

---
+++ src/core/ports/out/user.port.ts
@@ -0,0 +1,17 @@
+import { User } from "../../domain/user.model";
+
+export interface IUserRepository {
+  create(user: User): Promise<User>;
+
+  update(id: number, user: Partial<User>): Promise<User>;
+
+  changePassword(id: number, password: string): Promise<User>;
+
+  findById(id: number): Promise<User>;
+  findByEmail(email: string): Promise<User>;
+
+  softDelete(id: number): Promise<User>;
+
+  existsById(id: number): Promise<boolean>;
+  existsByEmail(email: string): Promise<boolean>;
+}
