c82b2ade1fbba13b9f06ca1523f54bad3fb08667
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 19:02:56 -0400

    fix(setting): updated findByName return type

--- src/core/ports/out/setting.repository.port.ts
+++ src/core/ports/out/setting.repository.port.ts
@@ -1,7 +1,8 @@
+import { Session } from "inspector/promises";
 import { Setting } from "../../domain/setting.model";
 
 export interface ISettingRepository {
   upsert(setting: Setting): Promise<Setting>;
 
-  findByName(name: string): Promise<String>;
+  findByName(name: string): Promise<Session>;
 }
