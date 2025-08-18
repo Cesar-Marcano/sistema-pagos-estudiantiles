e86e2c4dc5123ab74f9c80dcbde5cd3c0ef4f4c0
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 19:56:35 -0400

    feat(audit): added audit log port

---
+++ src/core/ports/out/auditLog.repository.port.ts
@@ -0,0 +1,23 @@
+import { Paginated } from "../../../shared/interfaces/paginated";
+import { AuditLog, AuditLogActions, Entities } from "../../domain/auditLog.model";
+
+export interface IAuditLogSearchCriteria {
+  query?: {
+    entity?: Entities;
+    entityId?: number;
+    action?: AuditLogActions;
+    createdById?: number;
+    createdAt?: Date;
+    deletedAt?: Date;
+  };
+  page: number;
+  pageSize: number;
+  includeDeleted: boolean;
+}
+
+export interface IAuditLog {
+  create(auditLog: AuditLog): Promise<AuditLog>;
+
+  search(criteria: IAuditLogSearchCriteria): Promise<Paginated<AuditLog>>;
+  findById(id: number): Promise<AuditLog>;
+}
