7c1f8d393e5680f94fed12238bca0ec531c34580
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-18 21:27:32 -0400

    feat(router): removed unused routes

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,13 +1,4 @@
 import { Router } from "express";
-import { authRouter } from "../routes/auth";
-import { parentRouter } from "../routes/parent";
-import { studentRouter } from "../routes/student";
-import { gradeRouter } from "../routes/grade";
 
 export const router = Router();
 
-// routes
-router.use("/auth", authRouter);
-router.use("/parent", parentRouter);
-router.use("/student", studentRouter);
-router.use("/grade", gradeRouter);
