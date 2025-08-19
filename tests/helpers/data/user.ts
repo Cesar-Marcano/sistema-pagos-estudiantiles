5986a1f8e07eb0d62064fe716eb2a0e4f6582d8c
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-19 16:21:26 -0400

    test(user): updates user data

--- tests/helpers/data/user.ts
+++ tests/helpers/data/user.ts
@@ -1,21 +1,31 @@
+import { Email } from "../../../src/core/datavalues/email.datavalue";
+import { Password } from "../../../src/core/datavalues/password.datavalue";
+import { Username } from "../../../src/core/datavalues/username.datavalue";
 import { User } from "../../../src/core/domain/user.model";
+import { IHasherService } from "../../../src/core/ports/out/services/hasher.service.port";
 
 export const sampleUserData = {
-  email: "my.test.email@email.com",
+  email: new Email("test@test.com"),
   password: "somePasswordLol123+",
   name: "Test User",
-  username: "test_user",
+  username: new Username("something"),
 };
 
-export const testNewUser = () =>
-  User.create(
+export const testNewUser = async (IHasherService: IHasherService) => {
+  const password = await Password.create(
+    sampleUserData.password,
+    IHasherService
+  );
+
+  return User.create(
     1,
     sampleUserData.username,
     sampleUserData.name,
-    sampleUserData.password,
+    password,
     sampleUserData.email,
     1
   );
+};
 
 export const testDbUser = () => {
   const now = new Date();
@@ -24,7 +34,7 @@ export const testDbUser = () => {
     1,
     sampleUserData.username,
     sampleUserData.name,
-    sampleUserData.password,
+    Password.fromHash("hashedPassword"),
     sampleUserData.email,
     1,
     now,
