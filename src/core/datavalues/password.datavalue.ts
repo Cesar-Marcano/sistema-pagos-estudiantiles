7f7f47a3d2583e80420c5285181e05103b295c96
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-19 15:44:50 -0400

    feat(core): refactors password value object

--- src/core/datavalues/password.datavalue.ts
+++ src/core/datavalues/password.datavalue.ts
@@ -1,14 +1,41 @@
-export class Password {
-  constructor(private readonly _value: string) {
-    const regExp =
-      /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*()_+{}:;\'"?/<,.~`|\\\]\[-])(?=.{8,})/;
+import { IHasherService } from "../ports/out/services/hasher.service.port";
 
-    if (!regExp.test(_value)) {
-      throw new Error("Weak password");
-    }
+export class Password {
+  private readonly _hash: string;
+
+  private constructor(hash: string) {
+    this._hash = hash;
   }
 
-  public get value(): string {
-    return this._value;
+  public static async create(
+    password: string,
+    hasherService: IHasherService
+  ): Promise<Password> {
+    if (!Password.validate(password)) {
+      throw new Error("Weak password");
+    }
+    const hash = await hasherService.hash(password);
+    return new Password(hash);
+  }
+
+  public static fromHash(hash: string): Password {
+    return new Password(hash);
+  }
+
+  public async compare(
+    password: string,
+    hasherService: IHasherService
+  ): Promise<boolean> {
+    return hasherService.compare(password, this._hash);
+  }
+
+  public get hash(): string {
+    return this._hash;
+  }
+
+  private static validate(password: string): boolean {
+    const regExp =
+      /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*()_+{}:;\'"?/<,.~`|\\\]\[-])(?=.{8,})/;
+    return regExp.test(password);
   }
 }
