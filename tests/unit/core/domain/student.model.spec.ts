673809b37375a41be1fd64f7371aa84ad56004ec
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-21 16:11:26 -0400

    test: adds tests for content updates

--- tests/unit/core/domain/student.model.spec.ts
+++ tests/unit/core/domain/student.model.spec.ts
@@ -1,4 +1,8 @@
-import { Student } from "../../../../src/core/domain/student.model";
+import { IdentificationDocument } from "../../../../src/core/datavalues/document.datavalue";
+import {
+  Student,
+  StudentStatus,
+} from "../../../../src/core/domain/student.model";
 import { testDbStudent, testNewStudent } from "../../../helpers/data/student";
 
 describe("User model", () => {
@@ -24,6 +28,103 @@ describe("User model", () => {
       expect(studentFromDB.id).toBe(1);
       expect(studentFromDB.createdAt).toBeInstanceOf(Date);
       expect(studentFromDB.updatedAt).toBeInstanceOf(Date);
+    });
+  });
+
+  describe("Discount updates", () => {
+    let oldUpdatedAt: Date;
+
+    beforeEach(() => {
+      oldUpdatedAt = student.updatedAt;
+      student.addDiscount(2);
+    });
+
+    it("should add discount", () => {
+      expect(student.discounts.includes(2)).toBeTruthy();
+      expect(student.updatedAt).not.toBe(oldUpdatedAt);
+    });
+
+    it("should remove a discount", () => {
+      student.removeDiscount(2);
+
+      expect(student.discounts.includes(2)).toBeFalsy();
+      expect(student.updatedAt).not.toBe(oldUpdatedAt);
+    });
+
+    describe("Content updates", () => {
+      it("should update the document", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newDocument = new IdentificationDocument("V-123456");
+        student.updateDocument(newDocument);
+
+        expect(student.document).toBe(newDocument);
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
+
+      it("should update the fullname", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newFullname = "John Doe";
+        student.updateFullname(newFullname);
+
+        expect(student.fullname).toBe(newFullname);
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
+
+      it("should update the grade", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newGrade = 5;
+        student.updateGrade(newGrade, null);
+
+        expect(student.grade).toBe(newGrade);
+        expect(student.gradeLevel).toBeNull();
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
+
+      it("should update the join grade", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newJoinGrade = 5;
+        student.updateJoinGrade(newJoinGrade, 2);
+
+        expect(student.joinGrade).toBe(newJoinGrade);
+        expect(student.joinGradeLevel).toBe(2);
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
+
+      it("should update the join period", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newJoinPeriod = 6;
+        student.updateJoinPeriod(newJoinPeriod);
+
+        expect(student.joinPeriod).toBe(newJoinPeriod);
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
+
+      it("should update the parent", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newParent = 1;
+        student.updateParent(newParent);
+
+        expect(student.parent).toBe(newParent);
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
+
+      it("should update the section", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newSection = "C";
+        student.updateSection(newSection);
+
+        expect(student.section).toBe(newSection);
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
+
+      it("should update the status", () => {
+        const oldUpdatedAt = student.updatedAt;
+        const newStatus = StudentStatus.Inactive;
+        student.updateStatus(newStatus);
+
+        expect(student.status).toBe(newStatus);
+        expect(student.updatedAt).not.toBe(oldUpdatedAt);
+      });
     });
   });
 });
