4aaba8906bfbe2887e77bddab7dc4cb10ad33b08
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-21 20:55:33 -0400

    feat(user): implements update password feature

---
+++ src/infrastructure/features/user/updatePassword.feature.ts
@@ -0,0 +1,22 @@
+import { User } from "../../../core/domain/user.model";
+import { UpdatePasswordDTO } from "../../../core/dto/user/update.dto";
+import { IUpdatePasswordFeature } from "../../../core/ports/in/user/updatePassword.port";
+import { IUserRepository } from "../../../core/ports/out/repositories/user.repository.port";
+import { IHasherService } from "../../../core/ports/out/services/hasher.service.port";
+
+export class UpdatePasswordFeature implements IUpdatePasswordFeature {
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly hasherService: IHasherService
+  ) {}
+
+  async execute(input: UpdatePasswordDTO): Promise<User> {
+    const user = await this.userRepository.findById(input.userId);
+
+    if (!user) throw new Error("UpdateNameFeature failed: user doesn't exist.");
+
+    user.changePassword(input.newPassword, this.hasherService);
+
+    return await this.userRepository.update(user);
+  }
+}
