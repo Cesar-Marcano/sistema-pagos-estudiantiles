771a4f3a0341339790b21cb8e741a3809add1bd7
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-22 18:21:50 -0400

    feat(student): added update dtos

---
+++ src/core/dto/student/update.dto.ts
@@ -0,0 +1,39 @@
+export class UpdateFullnameDTO {
+  constructor(private readonly id: number, private readonly fullname: string) {}
+}
+
+export class UpdateBirthdayDTO {
+  constructor(private readonly id: number, private readonly birthday: Date) {}
+}
+
+export class UpdateSectionDTO {
+  constructor(
+    private readonly id: number,
+    private readonly section: string | null
+  ) {}
+}
+
+export class UpdateGradeDTO {
+  constructor(private readonly id: number, private readonly grade: number) {}
+}
+
+export class UpdateGradeLevelDTO {
+  constructor(
+    private readonly id: number,
+    private readonly gradeLevel: number | null
+  ) {}
+}
+
+export class UpdateJoinGradeDTO {
+  constructor(
+    private readonly id: number,
+    private readonly joinGrade: number
+  ) {}
+}
+
+export class UpdateJoinGradeLevelDTO {
+  constructor(
+    private readonly id: number,
+    private readonly joinGradeLevel: number | null
+  ) {}
+}
