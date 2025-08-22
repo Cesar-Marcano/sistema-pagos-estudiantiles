f1ee426d94e2306e66c19b94b8aa1dfc99a986ff
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-21 21:36:57 -0400

    feat(user): validates role existence on update

--- src/infrastructure/features/user/updateRole.feature.ts
+++ src/infrastructure/features/user/updateRole.feature.ts
@@ -1,15 +1,24 @@
 import { User } from "../../../core/domain/user.model";
 import { UpdateRoleDTO } from "../../../core/dto/user/update.dto";
 import { IUpdateRoleFeature } from "../../../core/ports/in/user/updateRole.port";
+import { IRoleRepository } from "../../../core/ports/out/repositories/role.repository.port";
 import { IUserRepository } from "../../../core/ports/out/repositories/user.repository.port";
 
 export class UpdateRoleFeature implements IUpdateRoleFeature {
-  constructor(private readonly userRepository: IUserRepository) {}
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly roleRepository: IRoleRepository
+  ) {}
 
   async execute(input: UpdateRoleDTO): Promise<User> {
     const user = await this.userRepository.findById(input.userId);
 
     if (!user) throw new Error("UpdateRoleFeature failed: user doesn't exist.");
+
+    const roleExists = await this.roleRepository.roleExists(input.role);
+
+    if (!roleExists)
+      throw new Error("UpdateRoleFeature failed: role doesn't exist.");
 
     user.updateRole(input.role);
 
