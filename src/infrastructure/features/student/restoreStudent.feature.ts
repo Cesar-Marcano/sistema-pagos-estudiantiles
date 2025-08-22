638bd7941e3d657b2c3ac2df12f3314c8315d49b
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-22 18:18:02 -0400

    refactor(student): renames restore dto

--- src/infrastructure/features/student/restoreStudent.feature.ts
+++ src/infrastructure/features/student/restoreStudent.feature.ts
@@ -1,12 +1,12 @@
 import { Student } from "../../../core/domain/student.model";
-import { RestoreStudentDTO } from "../../../core/dto/student/restore.dto";
+import { FindStudentByIdDTO } from "../../../core/dto/student/findById.dto";
 import { IRestoreStudentFeature } from "../../../core/ports/in/student/restoreStudent.port";
 import { IStudentRepository } from "../../../core/ports/out/repositories/student.repository.port";
 
 export class RestoreStudentFeature implements IRestoreStudentFeature {
   constructor(private readonly studentRepository: IStudentRepository) {}
 
-  async execute(input: RestoreStudentDTO): Promise<Student> {
+  async execute(input: FindStudentByIdDTO): Promise<Student> {
     const student = await this.studentRepository.findById(input.studentId);
 
     if (!student)
