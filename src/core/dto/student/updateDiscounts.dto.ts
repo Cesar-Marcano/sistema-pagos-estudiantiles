9e7abbb6c05f2792161c40e6ffa390a785648b66
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-22 19:29:24 -0400

    fix(student): modifies discount properties

--- src/core/dto/student/updateDiscounts.dto.ts
+++ src/core/dto/student/updateDiscounts.dto.ts
@@ -1,7 +1,7 @@
 export class AddDiscountDTO {
-  constructor(public readonly id: number, private readonly discount: number) {}
+  constructor(public readonly id: number, public readonly discount: number) {}
 }
 
 export class RemoveDiscountDTO {
-  constructor(public readonly id: number, private readonly discount: number) {}
+  constructor(public readonly id: number, public readonly discount: number) {}
 }
