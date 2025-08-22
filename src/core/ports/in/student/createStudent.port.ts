fb2079ebb2338d68741795a681268b1bbc9809b9
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-21 22:12:54 -0400

    feat(student): makes create student return promise

--- src/core/ports/in/student/createStudent.port.ts
+++ src/core/ports/in/student/createStudent.port.ts
@@ -2,4 +2,4 @@ import { Feature } from "../../../../shared/interfaces/feature";
 import { Student } from "../../../domain/student.model";
 import { CreateStudentDTO } from "../../../dto/student/create.dto";
 
-export type ICreateStudentFeature = Feature<CreateStudentDTO, Student>;
+export type ICreateStudentFeature = Feature<CreateStudentDTO, Promise<Student>>;
