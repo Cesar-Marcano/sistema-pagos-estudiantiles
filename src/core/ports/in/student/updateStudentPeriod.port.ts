79da819532c1a16d116acd8ac291d1ae09bf2192
Cesar-Marcano <marcanocesar86@gmail.com>
2025-08-26 17:03:25 -0400

    feat(student): renamed update period port

--- src/core/ports/in/student/updateStudentPeriod.port.ts
+++
@@ -1,8 +0,0 @@
-import { Feature } from "../../../../shared/interfaces/feature";
-import { Student } from "../../../domain/student.model";
-import { UpdateStudentPeriodDTO } from "../../../dto/student/update.dto";
-
-export type IUpdateStudentPeriodFeature = Feature<
-  UpdateStudentPeriodDTO,
-  Promise<Student>
->;
